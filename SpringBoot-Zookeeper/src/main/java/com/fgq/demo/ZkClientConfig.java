7c9aec98fc8a550b14e1af2c585ca0bd3c2fa5ba
fgq233 <18217610763@163.com>
2023-01-07 03:54:37 +0800

    zk

---
+++ SpringBoot-Zookeeper/src/main/java/com/fgq/demo/ZkClientConfig.java
@@ -0,0 +1,29 @@
+package com.fgq.demo;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ZkClientConfig {
+
+
+    @Bean
+    public CuratorFramework createWithOptions(){
+        //重试策略
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(3000, 10);
+        CuratorFramework client = CuratorFrameworkFactory.builder()
+                .connectString("127.0.0.1:2181")
+                .sessionTimeoutMs(60 * 1000)     // 会话超时60s
+                .connectionTimeoutMs(15 * 1000)  // 连接超时30s
+                .retryPolicy(retryPolicy)
+                //.namespace("fgq")               // 命名空间，生成的节点会自动在前面带上
+                .build();
+        client.start();
+        return client;
+    }
+
+}
