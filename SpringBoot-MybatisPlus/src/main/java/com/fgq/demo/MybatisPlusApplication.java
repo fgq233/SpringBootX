a6e37ae0c5b591c02a3972cde392f749e14a712f
fgq233 <18217610763@163.com>
2023-01-28 15:27:32 +0800

    666666

--- SpringBoot-MybatisPlus/src/main/java/com/fgq/demo/MybatisPlusApplication.java
+++ SpringBoot-MybatisPlus/src/main/java/com/fgq/demo/MybatisPlusApplication.java
@@ -3,14 +3,13 @@ package com.fgq.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan("com.fgq.demo.mapper")
 @SpringBootApplication
-public class DemoApplication {
+public class MybatisPlusApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(MybatisPlusApplication.class, args);
     }
 
 
