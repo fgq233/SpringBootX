fa63280058e7cfb6923dc885afa571d363bec2f0
fgq233 <18217610763@163.com>
2023-01-28 23:23:16 +0800

    oauth2

--- SpringBoo-SpringSecurity-OAuth2/oauth2_server_jdbc/src/main/java/com/fgq/demo/ServerApplication.java
+++ SpringBoo-SpringSecurity-OAuth2/oauth2_server_jdbc/src/main/java/com/fgq/demo/ServerApplication.java
@@ -1,13 +1,14 @@
-package demo;
+package com.fgq.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SourceApplication {
+public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SourceApplication.class, args);
+        SpringApplication.run(ServerApplication.class, args);
     }
 
 
---
+++ SpringBoo-SpringSecurity-OAuth2/oauth2_source/src/main/java/com/fgq/demo/UserController.java
@@ -0,0 +1,21 @@
+package com.fgq.demo;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    /**
+     * 使用令牌 access_token 访问
+     * http://localhost:9002/user/getCurrentUser?access_token=713ea91e-80d7-4d48-95d0-0be601bf0ae5
+     */
+    @GetMapping("/getCurrentUser")
+    public Object getCurrentUser(Authentication authentication) {
+        return authentication.getPrincipal();
+    }
+
+}
