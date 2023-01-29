c6318c2001fc0b535a077e498ce1ca225c71203f
范光强 <18217610763@163.com>
2023-01-29 17:49:48 +0800

    oauth2

--- SpringBoo-SpringSecurity-OAuth2/oauth2_source/src/main/java/com/fgq/demo/UserController.java
+++ SpringBoo-SpringSecurity-OAuth2/oauth2_source/src/main/java/com/fgq/demo/UserController.java
@@ -1,9 +1,12 @@
 package com.fgq.demo;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/user")
@@ -16,6 +19,21 @@ public class UserController {
     @RequestMapping("/getCurrentUser")
     public Object getCurrentUser(Authentication authentication) {
         return authentication.getPrincipal();
+    }
+
+
+    /**
+     * 使用令牌 access_token 访问
+     * http://localhost:9002/user/getCurrentJwtUser?access_token=713ea91e-80d7-4d48-95d0-0be601bf0ae5
+     */
+    @RequestMapping("/getCurrentJwtUser")
+    public Object getCurrentJwtUser(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        String token = header.replaceAll("bearer ", "");
+        return Jwts.parser()
+                .setSigningKey("test_key".getBytes(StandardCharsets.UTF_8))
+                .parseClaimsJws(token)
+                .getBody();
     }
 
 }
