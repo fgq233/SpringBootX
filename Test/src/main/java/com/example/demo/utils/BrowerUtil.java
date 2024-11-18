ad834f2a1c3ed8f0f200aa699699add03d506486
fgq <18217610763@163.com>
2024-11-18 11:12:04 +0800

    浏览器

--- Test/src/main/java/com/example/demo/utils/BrowerUtil.java
+++ Test/src/main/java/com/example/demo/utils/BrowerUtil.java
@@ -10,7 +10,22 @@ public class BrowerUtil {
         if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
             try {
                 // 使用Desktop打开默认浏览器并导航到指定的URL
-                Desktop.getDesktop().browse(new URI("http://09thz.com/forum-60-" + num + ".html"));
+                Desktop.getDesktop().browse(new URI("xxx" + num + ".html"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Desktop 不支持打开浏览器或操作不被支持。");
+        }
+    }
+
+
+    public static void openBrowse(String url) {
+        // 检查Desktop类是否支持打开浏览器
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            try {
+                // 使用Desktop打开默认浏览器并导航到指定的URL
+                Desktop.getDesktop().browse(new URI(url));
             } catch (Exception e) {
                 e.printStackTrace();
             }
