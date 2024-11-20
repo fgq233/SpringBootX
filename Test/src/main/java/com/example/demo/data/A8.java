b9627a1b73a8043f7b5d6023565e4deeae4b2522
fgq <18217610763@163.com>
2024-11-20 09:08:15 +0800

    A

---
+++ Test/src/main/java/com/example/demo/data/A8.java
@@ -0,0 +1,54 @@
+package com.example.demo.data;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Optional;
+
+@Data
+public class A8 {
+
+    // 涨跌幅(%)
+    private BigDecimal pc;
+    // 当前价格(元)
+    private BigDecimal p;
+    // 最高价(元)
+    private BigDecimal h;
+    // 最低价(元)
+    private BigDecimal l;
+    // 换手(%)
+    private BigDecimal hs;
+    // 量比
+    private BigDecimal lb;
+    // 开盘价(元)
+    private BigDecimal o;
+    // 成交额(元)
+    private BigDecimal cje;
+    // 成交量(手)
+    private BigDecimal v;
+    // 涨跌额(元)
+    private BigDecimal ud;
+    // 振幅(%)
+    private BigDecimal zf;
+    // 数据更新时间
+    private String t;
+
+    @Override
+    public String toString() {
+        return "A8{" +
+                "涨跌幅=" + pc +
+                ", 当前价格=" + p +
+                ", 最高价=" + h +
+                ", 最低价=" + l +
+                ", 换手=" + Optional.ofNullable(zf).map(val -> hs + "%").orElse(null) +
+                ", 量比=" + lb +
+                ", 开盘价=" + o +
+                ", 成交额=" + Optional.ofNullable(cje).map(val -> val.divide(new BigDecimal("100000000"), 2, RoundingMode.UP) + "亿").orElse(null) +
+                ", 成交量=" + Optional.ofNullable(v).map(val -> val.divide(new BigDecimal("10000"), 2, RoundingMode.UP) + "万手").orElse(null) +
+                ", 涨跌额=" + ud +
+                ", 振幅=" + Optional.ofNullable(zf).map(val -> zf + "%").orElse(null) +
+                ", 数据更新时间='" + t + '\'' +
+                '}';
+    }
+}
--- Test/src/main/java/com/example/demo/utils/A.java
+++ Test/src/main/java/com/example/demo/utils/A.java
@@ -1,31 +1,43 @@
 package com.example.demo.utils;
 
+import com.alibaba.fastjson.JSON;
+import com.example.demo.data.A8;
 import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class A {
 
-    private static final String LICENCE = "D82E1726-7D49-4FEA-9392-DA2EB651B695";
+    private static final String LICENCE1 = "D82E1726-7D49-4FEA-9392-DA2EB651B695";
+    private static final String LICENCE2 = "77D8A44D-3E93-4A5A-8036-E8A505DEDD64";
     // 获取证书
     private static final String GET_LICENCE = "http://www.mairui.club/getlicence.html";
-    // 历史成交
-    private static final String URL_LS = "http://api.mairui.club/hsmy/lscjt/000066/" + LICENCE;
+    // 分时
+    private static final String URL_LS1 = "http://api.mairui.club/zs/sssj/000066/";
+    private static final String URL_LS2 = "http://api.mairui.club/hszb/fsjy/000066/dn/";
+    private static final String URL_LS3 = "http://api.mairui.club/zs/fsjy/000066/dn/";
 
 
     public static void main(String[] args) throws IOException {
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpGet get = new HttpGet(URL_LS);
-        CloseableHttpResponse response = client.execute(get);
-        HttpEntity httpEntity = response.getEntity();
-        String s = EntityUtils.toString(httpEntity, StandardCharsets.UTF_8);
-        System.out.println(s);
+        HttpClient client = new DefaultHttpClient();
+        HttpGet get = new HttpGet(URL_LS1 + LICENCE1);
+        HttpResponse response = client.execute(get);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
+            HttpEntity httpEntity = response.getEntity();
+            String str = EntityUtils.toString(httpEntity, StandardCharsets.UTF_8);
+            A8 a8 = JSON.parseObject(str, A8.class);
+            System.out.println(a8);
+        } else {
+            System.out.println("请求失败");
+        }
+
     }
 
 
